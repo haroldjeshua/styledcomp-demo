18c6027a97bab3aaf380ff3862e6a8974292483f
Harold Varde <harvardii15@gmail.com>
2022-11-22 14:36:51 +0800

    init commit + setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+export default function Header() {
+  return (
+    <Header>
+      <h1>Hubble</h1>
+    </Header>
+  );
+}
